549b9ba60b31382c3ca37820ecfd74fcf670fa37
“malcolmmutare” <mutarem@gmail.com>
2024-01-06 23:10:14 +0200

    Create Header, Library, and Box Components + Initial Styling

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import Header from '@/components/Header'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-green-500'>
-      Main Content
+    <div className="
+     bg-neutral-900
+      rounded-lg
+      h-full
+      w-full
+      overflow-hidden
+      overflow-y-auto
+    "
+    >
+      <Header>
+        Header
+      </Header>
+      
     </div>
   )
 }
