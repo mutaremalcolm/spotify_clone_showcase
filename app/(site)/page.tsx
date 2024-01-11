788e30660272e3be91282464ac2a05d784a465ef
“malcolmmutare” <mutarem@gmail.com>
2024-01-11 13:53:58 +0200

    Complete Song Upload to Library Functionality

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,15 @@
-"use client";
 
+import getSongs from '@/actions/getSongs';
 import Header from '@/components/Header'
 import ListItem from '@/components/ListItem';
-import Image from 'next/image'
+import Image from 'next/image';
+import PageContent from './components/PageContent';
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs =  await getSongs();
+
   return (
     <div className="
      bg-neutral-900
@@ -51,9 +56,7 @@ export default function Home() {
             Newest Songs
             </h1>
         </div>
-        <div>
-          List of Songs
-        </div>
+        <PageContent songs={songs} />
       </div>
     </div>
   )
